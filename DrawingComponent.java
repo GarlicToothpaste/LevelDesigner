93eba0bc25a29a76dfb201cc5764e79f406570f7
Adrian Danao <Adrian Danao>
2017-10-31 16:46:21 +0800

    added a drag function

--- DrawingComponent.java
+++ DrawingComponent.java
@@ -8,8 +8,9 @@ public class DrawingComponent extends JComponent{
 	int width, height;
 	Node node1;
 	Buttons addNode,addEdge,saveLevel;
-	private ArrayList<Shape> shapes = new ArrayList<>();
+	ArrayList<Node> nodes = new ArrayList<>();
     private Paint Background;
+    double mx,my;
 
 	public DrawingComponent(int x, int y){
 		width=x;
@@ -19,26 +20,52 @@ public class DrawingComponent extends JComponent{
 		addNode =new Buttons(20,20,120,30,Color.WHITE,"Add Node");
 		addEdge =new Buttons(150,20,120,30,Color.WHITE,"Add Edge");
 		saveLevel = new Buttons(280,20,140,30,Color.WHITE,"Save Level");
-
+		//node = new Node(200,200,40,Color.WHITE);
+       	this.addMouseListener(new bListener());
+       	this.addMouseMotionListener(new bListener());
 	}
-	protected void paintComponent (Graphics g){
+	public void paintComponent (Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
        	g2d.setPaint(Background);
        	g2d.fillRect(0,0,width,height);
 
        	g2d.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON));
        	
-       	node1.draw(g2d);
+       	//node1.draw(g2d);
        	addNode.draw(g2d);
        	addEdge.draw(g2d);
        	saveLevel.draw(g2d);
-
-       	this.addMouseListener(new bListener());
+       	drawNodes(g2d);
+       	repaint();
 	}
+		public class bListener extends MouseAdapter{
+			
+			public void mouseClicked(MouseEvent e){
 
-		public class bListener implements MouseListener{
-			public void mouseReleased(MouseEvent e){
+			}
+			public void mouseDragged(MouseEvent e){
+				if(checkNodes(mx,my)==true){
+					int selectedNode = getCurrentNode(mx,my);
+					//Gets the original Coordinates of the selected node
+					double prevX = (nodes.get(selectedNode).getX());
+					double prevY = (nodes.get(selectedNode).getY());
+					//Gets the difference of the new coordinates to the old mouse coordinates
+					double diffX = e.getX()-mx;
+					double diffY = e.getY()-my;
+					//Adds the of the old coordiantes to the diffX and diffY
+					double newX = prevX+diffX;
+					double newY = prevY+diffY;
+					//Updates the drawing
+					nodes.get(selectedNode).updateX(newX);
+					nodes.get(selectedNode).updateY(newY);
+					//Updates mx and my
+					mx = e.getX();
+					my = e.getY();
+					//System.out.println("mx: " + mx+ "my" + my);
+					//repaint();
+					System.out.println(getCurrentNode(mx,my));
 
+				}
 			}
 			public void mouseEntered(MouseEvent e){
 
@@ -46,25 +73,88 @@ public class DrawingComponent extends JComponent{
 			public void mouseExited(MouseEvent e){
 
 			}
-			public void mouseClicked(MouseEvent e){
+			public void mouseMoved(MouseEvent e){
 
 			}
 			public void mousePressed(MouseEvent e){
-				int mx = e.getX();
-				int my = e.getY();
+
+				mx = e.getX();
+				my = e.getY();
+				//If the mouse is clicked inside the buttons
 				if(my>=20 && my<=50){
 					//If add Node is Clicked
 					if(mx>=20 && mx<=140){
+						addNode();
+						repaint();
 						System.out.println("Add Node Button Clicked!");
 					}
 					//If add Edge is Clicked
 					if(mx>=150 && mx<=270){
-						System.out.println("Edd Edge Button Clicked!");
+						System.out.println("Add Edge Button Clicked!");
 					}
+					//If Save Level is Clicked!
 					if(mx>=280 && mx<=420){
 						System.out.println("Save Level Button Clicked!");
 					}
 				}
+
+
+				//If the mouse is clicked inside the node
+				// if(node1.isInsideNode(mx,my) == true){
+				// 	System.out.println("Mouse Inside node!");
+				// }
+
+				//If the mouse is clicked, it checks if the mouse is in a node.
+				if(checkNodes(mx,my)==true){
+					System.out.println("Mouse Inside Node!");
+				}
 			}
+			public void mouseReleased(MouseEvent e){
+
+			}
+			public void mouseWheelMoved(MouseEvent e){
+
+			}
+		}
+		//Draws the nodes in the arraylist
+		public void drawNodes (Graphics2D g2d){
+			for (int i=0; i<=(nodes.size()-1); i++) {
+				(nodes.get(i)).draw(g2d);
+			}
+		}
+		//Adds a node to the arraylist.
+		public void addNode (){
+			Node node;
+			int indexNumber = nodes.size();
+			node = new Node(200,200,40,Color.WHITE);
+			nodes.add(indexNumber,node);
+			System.out.println(nodes.size());
+		}
+		//Checks if mous is inside node.
+		public boolean checkNodes(double xPos, double yPos){
+			boolean var = false;
+			for (int i=0; i<=(nodes.size()-1); i++ ){
+				if((nodes.get(i)).isInsideNode(xPos,yPos)== true){
+					var = true;
+					break;
+				}
+
+			}
+			return var;
+		}
+		public int getCurrentNode(double xPos, double yPos){
+			boolean var = false;
+			int currNode = -1;
+			for (int i=0; i<=(nodes.size()-1); i++ ){
+				if((nodes.get(i)).isInsideNode(xPos,yPos)== true){
+					var = true;
+					if (var==true){
+						currNode = i;
+					}
+					break;
+				}
+
+			}
+			return currNode;
 		}
 }
--- Node.java
+++ Node.java
@@ -10,12 +10,32 @@ public class Node{
         radius = r;
         fillColor = fc;
     }
-
+    public void updateX(double x){
+        this.x = x;
+    }
+    public void updateY(double y){
+        this.y = y;
+    }
     public void draw(Graphics2D g2d){
 		Ellipse2D.Double circle = new Ellipse2D.Double(x,y,radius*2,radius*2);
 		g2d.setPaint(fillColor);
 		g2d.fill(circle);
 		g2d.draw(circle);
+    }
+    public boolean isInsideNode(double xPos, double yPos){
+        boolean var = false;
+        if(x<=xPos && xPos<= x+(radius*2)){
+            if (y<= yPos && yPos <= y+(radius*2)){
+                var = true;
+            }
+        }
+        return var;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
     }
 
 }
